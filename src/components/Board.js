8520ac7f6e1038c7a84a1242989dc100dc150edf
msolorio <msolorio789@gmail.com>
2017-09-04 01:01:15 -0700

    render board squares

--- src/components/Board.js
+++ src/components/Board.js
@@ -8,11 +8,40 @@ export default class Board extends Component {
     super(props);
   }
 
-  render() {
+  renderSquare(i) {
+
+    const x = i % 8;
+    const y = Math.floor(i / 8);
+    // if x + y is an odd number
+    const black = (x + y) % 2 === 1;
+    const [knightX, knightY] = this.props.knightPosition;
+    const squareContents = (x === knightX && y === knightY) ? <Knight /> : null;
+
     return (
-      <Square black>
-        <Knight/>
-      </Square>
+      <div key={i}
+        style={{ width: '12.5%', height: '12.5%' }}>
+        <Square black={black}>
+          {squareContents}
+        </Square>
+      </div>
+    );
+  }
+
+  render() {
+    const squares = [];
+
+    for (let i=0; i<64; i++) {
+      squares.push(this.renderSquare(i));
+    }
+    return (
+      <div style={{
+        width: '100%',
+        height: '100%',
+        display: 'flex',
+        flexWrap: 'wrap'
+      }}>
+      {squares}
+    </div>
     );
   }
 };
